cce2a936d155b08fca0b714ed3dc55afe9bb71ca
yueting <ytliaodev@gmail.com>
2025-06-30 15:20:42 -0400

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,26 +1,27 @@
 # In terraform/eks.tf
 
-# This module creates a complete, production-ready EKS cluster.
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.10.0"
 
   cluster_name    = "cloud-shop-cluster"
-  cluster_version = "1.29" # Use a recent, stable Kubernetes version
+  cluster_version = "1.29"
 
-  # This connects the EKS cluster to the VPC we created in vpc.tf.
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets # Deploy worker nodes in private subnets for security
+  subnet_ids = module.vpc.private_subnets
 
-  # This section defines the group of EC2 instances that will be our worker nodes.
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  # This is the single, correct line to grant admin permissions.
+  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_groups = {
-    # The name of our node group
     main_nodes = {
-      instance_types = ["t3.small"] # Use t3.small to avoid resource issues
-
-      min_size     = 1 # Start with 1 node
-      max_size     = 3 # Allow scaling up to 3 nodes
-      desired_size = 2 # Keep 2 nodes running for high availability
+      instance_types = ["t3.small"]
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
   }
 }
