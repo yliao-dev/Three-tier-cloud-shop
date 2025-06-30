21b73ce99b2d342052f91ff821953ef098c671de
yueting <ytliaodev@gmail.com>
2025-06-29 22:04:02 -0400

    provider

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -22,6 +22,7 @@ resource "aws_ecr_repository" "app_repos" {
 
   name                 = each.key
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+# In terraform/providers.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1" # Set to your desired region
+}
