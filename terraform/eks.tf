334566b1193f6a5a90e5b73efdad2166cafff658
yueting <ytliaodev@gmail.com>
2025-07-02 19:22:50 -0400

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -26,7 +26,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     main_nodes = {
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
       min_size     = 1
       max_size     = 3
       desired_size = 2
