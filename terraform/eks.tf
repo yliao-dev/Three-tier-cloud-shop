1a6b67f6f6d4c33cbcd9bd1ef57c04b03e3b361c
yueting <ytliaodev@gmail.com>
2025-07-03 16:35:08 -0400

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -4,7 +4,7 @@ module "eks" {
   version = "20.10.0"
 
   cluster_name    = "cloud-shop-cluster"
-  cluster_version = "1.29"
+  cluster_version = "1.30"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
